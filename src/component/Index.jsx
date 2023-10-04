66d343e839cb1d94eb68cda1d176c1da01feba86
fptcsm <gianglamweb@gmail.com>
2023-10-05 01:29:26 +0700

    commit

---
+++ src/component/Index.jsx
@@ -0,0 +1,208 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div className="_li">
+      <div className="_li _30l2 _-f_" id="u_0_0_e6">
+        <div className="_3b5k" id="bizsitePageContainer">
+          <div>
+            <div className="_9att">
+              <div className="_amch">
+                <div className="_9atw">
+                  <div className="x1gzqxud x1lq5wgf xgqcy7u x30kzoy x9jhf4c x1kmqopl x5yr21d xh8yej3">
+                    <div className="x78zum5 xdt5ytf x5yr21d xedcshv x1t2pt76 xh8yej3">
+                      <div className="x9f619 x78zum5 x1iyjqo2 x5yr21d x2lwn1j x1n2onr6 xh8yej3">
+                        <div className="xw2csxc x1odjw0f xh8yej3 x18d9i69">
+                          <div className="xjm9jq1 xg01cxk x47corl xh8yej3 x1jyxor1" />
+                          <div className="_9atv">
+                            <div className="_9atu">
+                              <div className="_9t3t">
+                                <i
+                                  alt=""
+                                  data-visualcompletion="css-img"
+                                  className="img sp_b0sskVdMBwm sx_89f9ee"
+                                />
+                              </div>
+                            </div>
+                            <div className="_9pex">
+                              <span className="x8t9es0 x1fvot60 xo1l8bm xxio538 x108nfp6 xq9mrsl x2b8uid x1h4wwuj xeuugli">
+                                Log in to access your professional tools
+                              </span>
+                            </div>
+                          </div>
+                          <div className="_9rq5">
+                            <div className="xeuugli x2lwn1j x6s0dn4 x78zum5 xdt5ytf x1iyjqo2 xozqiw3 xavht8x">
+                              <div className="_9atx">
+                                <div
+                                  className="x193iq5w xxymvpz x78zum5 x1iyjqo2 xs83m0k"
+                                  role="none"
+                                >
+                                  <div
+                                    aria-busy="false"
+                                    className="x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x16tdsg8 xggy1nq x1ja2u2z x1t137rt x6s0dn4 x1ejq31n xd10rxx x1sy0etr x17r0tee xdl72j9 x1q0g3np x193iq5w x1n2onr6 x1hl2dhg x87ps6o xxymvpz xlh3980 xvmahel x1lku1pv xhk9q7s x1otrzb0 x1i1ezom x1o6z2jb xo1l8bm x108nfp6 xas4zb2 x1y1aw1k xwib8y2 x1pi30zi x1ye3gou x78zum5 x1iyjqo2 xs83m0k"
+                                    role="button"
+                                    tabIndex={0}
+                                  >
+                                    <span className="x8t9es0 x1fvot60 xxio538 x1heor9g xq9mrsl x1h4wwuj x1pd3egz xeuugli xh8yej3">
+                                      <div className="x78zum5">
+                                        <div className="xeuugli x2lwn1j x6s0dn4 x78zum5 x1q0g3np x1iyjqo2 xozqiw3 x19lwn94 x1hc1fzr x13dflua x6o7n8i xxziih7 x12w9bfk xl56j7k xh8yej3">
+                                          <div
+                                            className="x3nfvp2 x120ccyz x1heor9g x2lah0s x1c4vz4f"
+                                            role="presentation"
+                                          >
+                                            <div
+                                              className="xtwfq29"
+                                              style={{
+                                                width: "16px",
+                                                height: "16px",
+                                                WebkitMaskImage:
+                                                  'url("https://static.xx.fbcdn.net/rsrc.php/v3/yZ/r/2Y91aEEwqNW.png")',
+                                                WebkitMaskPosition:
+                                                  "-251px -212px",
+                                              }}
+                                            />
+                                          </div>
+                                          <div className="x8t9es0 x1fvot60 xxio538 x1heor9g xuxw1ft x6ikm8r x10wlt62 xlyipyv x1h4wwuj x1pd3egz xeuugli">
+                                            Continue with Giang Truong
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </span>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="_9atx" />
+                            </div>
+                          </div>
+                          <div className="xjm9jq1 xg01cxk x47corl xh8yej3 x1y332i5" />
+                        </div>
+                        <div className="x10wjd1d x47corl x10l6tqk x19991ni x13dflua xwji4o3 xh8yej3 xg01cxk x67dex8 x13vifvy" />
+                        <div className="x10wjd1d x47corl x10l6tqk x19991ni x13dflua xwji4o3 xh8yej3 xg01cxk x1ta9b4f x1ey2m1c" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/*  */}
+              <div className="_9pjw">
+                <div className="_9pjx">
+                  <a
+                    className="xt0psk2 x1hl2dhg xt0b8zv x8t9es0 x1fvot60 xxio538 xjnfcd9 xq9mrsl x1yc453h x1h4wwuj x1fcty0u"
+                    href="https://developers.facebook.com/"
+                  >
+                    <div
+                      aria-level={4}
+                      className="x8t9es0 x1fvot60 xxio538 x140t73q xeaf4i8 x1yc453h x1h4wwuj x1fcty0u xeuugli"
+                      role="heading"
+                    >
+                      Developer
+                    </div>
+                  </a>
+                </div>
+                <div className="_9pjx">
+                  <a
+                    className="xt0psk2 x1hl2dhg xt0b8zv x8t9es0 x1fvot60 xxio538 xjnfcd9 xq9mrsl x1yc453h x1h4wwuj x1fcty0u"
+                    href="/privacy/explanation"
+                  >
+                    <div
+                      aria-level={4}
+                      className="x8t9es0 x1fvot60 xxio538 x140t73q xeaf4i8 x1yc453h x1h4wwuj x1fcty0u xeuugli"
+                      role="heading"
+                    >
+                      Privacy
+                    </div>
+                  </a>
+                </div>
+                <div className="_9pjx">
+                  <a
+                    className="xt0psk2 x1hl2dhg xt0b8zv x8t9es0 x1fvot60 xxio538 xjnfcd9 xq9mrsl x1yc453h x1h4wwuj x1fcty0u"
+                    href="/policies/"
+                  >
+                    <div
+                      aria-level={4}
+                      className="x8t9es0 x1fvot60 xxio538 x140t73q xeaf4i8 x1yc453h x1h4wwuj x1fcty0u xeuugli"
+                      role="heading"
+                    >
+                      Terms
+                    </div>
+                  </a>
+                </div>
+                <div className="_9pjx">
+                  <a
+                    className="xt0psk2 x1hl2dhg xt0b8zv x8t9es0 x1fvot60 xxio538 xjnfcd9 xq9mrsl x1yc453h x1h4wwuj x1fcty0u"
+                    href="/policies/cookies/"
+                  >
+                    <div
+                      aria-level={4}
+                      className="x8t9es0 x1fvot60 xxio538 x140t73q xeaf4i8 x1yc453h x1h4wwuj x1fcty0u xeuugli"
+                      role="heading"
+                    >
+                      Cookies
+                    </div>
+                  </a>
+                </div>
+                <div className="_9pjx">
+                  <a
+                    className="xt0psk2 x1hl2dhg xt0b8zv x8t9es0 x1fvot60 xxio538 xjnfcd9 xq9mrsl x1yc453h x1h4wwuj x1fcty0u"
+                    href="/help/?ref=href037"
+                  >
+                    <div
+                      aria-level={4}
+                      className="x8t9es0 x1fvot60 xxio538 x140t73q xeaf4i8 x1yc453h x1h4wwuj x1fcty0u xeuugli"
+                      role="heading"
+                    >
+                      Help
+                    </div>
+                  </a>
+                </div>
+                <div className="_9pjx">
+                  <div
+                    aria-level={4}
+                    className="x8t9es0 x1fvot60 xxio538 xto31z9 xeaf4i8 x1yc453h x1h4wwuj x1fcty0u xeuugli"
+                    role="heading"
+                  >
+                    © 2023 Meta
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        id="pagelet_dock"
+        data-referrer="pagelet_dock"
+        data-visualcompletion="ignore"
+      >
+        <div className="_48gf fbDockWrapper fbDockWrapperRight" id="u_0_3_Tj">
+          <div className="fbDock clearfix">
+            <div className="clearfix nubContainer rNubContainer">
+              <div id="u_0_6_HG">
+                <div className="uiToggle _50-v fbNub _4up">
+                  <a
+                    className="fbNubButton"
+                    tabIndex={0}
+                    aria-label="Translations"
+                    href="https://www.facebook.com/translations/?ref=trans_nub"
+                    rel="toggle"
+                    data-hover="tooltip"
+                    data-tooltip-content="Translations"
+                    data-tooltip-alignh="right"
+                  >
+                    <i className="img sp_goSuTLKMvJk sx_3c39e3" />
+                  </a>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="_2xwp">
+          <div id="sticky-upload-hook" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
